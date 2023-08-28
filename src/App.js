b0527631fc8dc08a867c52d9e44b13cadd6b9749
Aravind Vallampatla <aravindvallampatla333@gmail.com>
2023-08-28 11:17:27 -0400

    ui

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React, { useState } from "react";
 import "./App.css";
 import TodoInput from "./components/TodoInput";
-import Todolist from "./components/TodoList";
+import TodoList from "./components/TodoList";
 import ProfileDropdown from "./components/ProfileDropdown";
 
 function App() {
   const initialProfiles = [
     {
       name: "Chandler",
+      profilePic:
+        "https://abuwjaawap.cloudimg.io/v7/_lgbtqnation-assets_/assets/2019/10/matthew-perry-chandler-bing-gay.jpg?auto=format&auto=compress&crop=faces&fit=crop&gravity=face&w=1200&h=805",
       tasks: [],
     },
     {
       name: "Monica",
+      profilePic:
+        "https://hips.hearstapps.com/goodhousekeeping-uk/main/embedded/14741/monica_friends_-_best_moments_-_funniest_character_-_good_housekeeping.jpg",
       tasks: [],
     },
     {
       name: "Joey",
+      profilePic:
+        "https://i.pinimg.com/originals/41/82/f1/4182f162ed8d32b7b0e9cbd5f841716f.jpg",
       tasks: [],
     },
   ];
@@ -25,26 +31,32 @@ function App() {
 
   const addTask = (inputText) => {
     if (inputText.trim() !== "") {
-      const updatedProfiles = [...profiles];
-      updatedProfiles[activeProfile].tasks.push({
-        text: inputText,
-        completed: false,
+      setProfiles((prevProfiles) => {
+        const updatedProfiles = [...prevProfiles];
+        updatedProfiles[activeProfile].tasks.push({
+          text: inputText,
+          completed: false,
+        });
+        return updatedProfiles;
       });
-      setProfiles(updatedProfiles);
     }
   };
 
   const deleteTask = (index) => {
-    const updatedProfiles = [...profiles];
-    updatedProfiles[activeProfile].tasks.splice(index, 1);
-    setProfiles(updatedProfiles);
+    setProfiles((prevProfiles) => {
+      const updatedProfiles = [...prevProfiles];
+      updatedProfiles[activeProfile].tasks.splice(index, 1);
+      return updatedProfiles;
+    });
   };
 
   const toggleTaskCompleted = (index) => {
-    const updatedProfiles = [...profiles];
-    updatedProfiles[activeProfile].tasks[index].completed =
-      !updatedProfiles[activeProfile].tasks[index].completed;
-    setProfiles(updatedProfiles);
+    setProfiles((prevProfiles) => {
+      const updatedProfiles = [...prevProfiles];
+      updatedProfiles[activeProfile].tasks[index].completed =
+        !updatedProfiles[activeProfile].tasks[index].completed;
+      return updatedProfiles;
+    });
   };
 
   const onChangeProfile = (index) => {
@@ -53,20 +65,20 @@ function App() {
 
   return (
     <div className="main-container">
-      <header className="header">
+      <div className="header">
         <ProfileDropdown
           profiles={profiles}
           activeProfile={activeProfile}
           onChangeProfile={onChangeProfile}
         />
-      </header>
+      </div>
 
       <div className="center-container">
         <TodoInput addList={addTask} />
 
         <div className="tasks">
           {profiles[activeProfile].tasks.map((task, index) => (
-            <Todolist
+            <TodoList
               key={index}
               item={task.text}
               completed={task.completed}
--- src/components/ProfileDropdown.js
+++ src/components/ProfileDropdown.js
@@ -1,17 +1,58 @@
 import React from "react";
+import Select from "react-select";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
 
 function ProfileDropdown({ profiles, activeProfile, onChangeProfile }) {
+  const [profile, setProfile] = React.useState(profiles[0]);
+  const [profileList, showProfileList] = React.useState(false);
+  const handleProfileSelect = (val) => {
+    console.log(val, "val");
+    setProfile(profiles[val]);
+    showProfileList(!profileList);
+  };
+  // console.log(profileList);
   return (
-    <select
-      value={activeProfile}
-      onChange={(e) => onChangeProfile(parseInt(e.target.value))}
-    >
-      {profiles.map((profile, index) => (
-        <option key={index} value={index}>
-          {profile.name}
-        </option>
-      ))}
-    </select>
+    <div className="custom-profile-select">
+      <div className="active-profile">
+        <FontAwesomeIcon
+          icon={faAngleDown}
+          onClick={() => showProfileList(!profileList)}
+        />
+        <img className="profile-picture" src={profile.profilePic} />
+        <p>{profile.name}</p>
+      </div>
+      {profileList &&
+        profiles.map((val, index) => {
+          return (
+            val.name !== profile.name && (
+              <div
+                className="profile-selction-div"
+                onClick={() => handleProfileSelect(index)}
+                key={index}
+                value={index}
+              >
+                <img
+                  className="profile-picture"
+                  src={val.profilePic}
+                  // alt="image not found"
+                />
+                <p>{val.name}</p>
+              </div>
+            )
+          );
+        })}
+    </div>
+    // <select
+    //   value={activeProfile}
+    //   onChange={(e) => onChangeProfile(parseInt(e.target.value))}
+    // >
+    //   {profiles.map((profile, index) => (
+    //     <option src="./joey.jpg" key={index} value={index}>
+    //       {profile.name}
+    //     </option>
+    //   ))}
+    // </select>
   );
 }
 
